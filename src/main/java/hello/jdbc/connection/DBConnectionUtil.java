2ee3b34fbb5670696643f8721ca7302c08b5b3e7
SeongbinPark <binni0515@gmail.com>
2022-09-21 22:17:11 +0900

    1-4.DB연결(java.sql.Connection인터페이스, 각 벤더사의 드라이버에 Connection 구현체, DriverManger.getConnection()을 통한 Connection 획득)

---
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,25 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) { // CheckedException을
+            // UncheckedException(RuntimeException)으로 바꿔서 던진다.
+            throw new IllegalStateException(e); //왜 바꾸는지 알았는데..
+        }
+    }
+
+}
